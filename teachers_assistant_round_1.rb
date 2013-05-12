4bcf1967dcd9ad19682095a968cb878e5ad0214d
Michael Raimondi <zeno@pobox.com>
2013-05-11 22:31:45 -0400

    Implement teach-report

--- teachers_assistant_round_1.rb
+++ teachers_assistant_round_1.rb
@@ -13,16 +13,20 @@ class Student
     first_name + " " + last_name
   end
 
+  def total_score
+    total = 0
+    @scores.each do |score|
+      total += score
+    end
+    total
+  end
+
   def accumulate_grade(score)
     @scores << score.to_i
   end
 
   def average_score
-    total = 0
-    @scores.each do |score|
-      total += score
-    end
-    total.to_f / scores.length
+    total_score.to_f / scores.length
   end
 
   def average_grade
@@ -60,6 +64,44 @@ class Cohort
     @students.sort { |a,b| a.last_name.downcase <=> b.last_name.downcase }
   end
 
+  def all_scores
+    scores = []
+    @students.each do |student|
+      student.scores.each do |score|
+        scores << score
+      end
+    end
+    scores
+  end
+
+  def total_score
+    total = 0
+    all_scores.each do |score|
+      total += score
+    end
+    total
+  end
+
+  def average_score
+    total = total_score
+    total_score.to_f / all_scores.length
+  end
+
+  def standard_deviation
+    average = average_score
+    diffs = []
+    all_scores.each do |score|
+      diff = (score - average)
+      diff = diff * diff
+      diffs << diff
+    end
+    total_diffs = 0
+    diffs.each do |diff|
+      total_diffs += diff
+    end
+    Math.sqrt(total_diffs / diffs.length)
+  end
+
   def stringify(options = {})
     str = "Class: #{name}\n\n"
     students_by_last_name.each do |student|
@@ -71,10 +113,15 @@ class Cohort
         end
         str += "\n"
       end
-      str += "Average score: #{student.average_score}\n" if options[:averages]
+      str += "Average score: #{student.average_score.round(1)}\n" if options[:averages]
       str += "Average grade: #{student.average_grade}\n" if options[:average_grades]
       str += "\n"
     end
+    str += "Cohort-wide total score: #{total_score}\n" if options[:agg_total_scores]
+    str += "Cohort-wide average score: #{average_score.round(1)}\n" if options[:agg_average_score]
+    str += "Cohort-wide minimum score: #{all_scores.min}\n" if options[:agg_min_score]
+    str += "Cohort-wide maximum score: #{all_scores.max}\n" if options[:agg_max_score]
+    str += "Cohort-wide standard deviation: #{standard_deviation.round(2)}\n" if options[:agg_standard_deviation]
     str
   end
 
@@ -116,4 +163,5 @@ class Parser
 end
 
 c = Parser.parse_csv()
-c.to_txt_file ( {grades: true, averages: true, average_grades: :true} )
+c.to_txt_file ( {grades: true, averages: true, average_grades: :true, agg_average_score: :true, 
+                  agg_min_score: true, agg_max_score: true, agg_standard_deviation: true} )
