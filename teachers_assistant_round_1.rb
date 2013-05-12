3017e7af34ac033fb79c93a8ec4eac2485f7e2be
Michael Raimondi <zeno@pobox.com>
2013-05-12 01:19:54 -0400

    Implement noncore challenge

--- teachers_assistant_round_1.rb
+++ teachers_assistant_round_1.rb
@@ -117,7 +117,7 @@ class Cohort
       str += "Average grade: #{student.average_grade}\n" if options[:average_grades]
       str += "\n"
     end
-    str += "Cohort-wide total score: #{total_score}\n" if options[:agg_total_scores]
+    str += "Cohort-wide total score: #{total_score}\n" if options[:agg_total_score]
     str += "Cohort-wide average score: #{average_score.round(1)}\n" if options[:agg_average_score]
     str += "Cohort-wide minimum score: #{all_scores.min}\n" if options[:agg_min_score]
     str += "Cohort-wide maximum score: #{all_scores.max}\n" if options[:agg_max_score]
@@ -138,30 +138,101 @@ class Cohort
 
 end
 
-class Parser
+class Interface
 
-  PATH = "sample_data.csv"
+  def self.dispatch(args)
+    stdout = false
+    if args.length == 0
+      Interface.print_help
+      return
+    elsif args.length == 1
+      begin
+        if (args[0] =~ /^-h/) || (args[0] =~ /^--h/)
+          Interface.print_help
+          return
+        end
+        cohort = Interface.parse_csv(args[0])
+        cohort.to_txt_file()
+      rescue
+        Interface.print_help
+        return
+      end
+    else
+      if (args[0] == "-stdout") || (args[0] == "-s")
+        stdout = true
+        args.shift()
+      end
+      file = args.shift()
+      options = {}
+      args.each do |arg|
+        key = arg.to_sym
+        options.merge!( {key => true})
+      end
+      begin
+        cohort = Interface.parse_csv(file)
+        stdout ? cohort.to_stdout(options) : cohort.to_txt_file(options) 
+      rescue
+        Interface.print_help
+      end
+    end
+  end
 
-  def self.parse_csv
+  def self.print_help
+    help = <<-DELIMIT
+      usage: ruby teachers_assistant_round_1.rb [-h|--help]
+             [-s|--stdout]<filename> <options>
+
+      Valid options are:
+        grades
+        averages
+        average_grades
+        agg_total_score
+        agg_average_score
+        agg_min_score
+        agg_max_score
+        agg_standard_deviation
+        DELIMIT
+    puts help
+  end
+
+  def self.parse_csv(path)
+    if !File.exists?(path)
+      puts "Please enter a valid file name"
+      print_help
+      return
+    elsif File.extname(path) != ".csv"
+      puts "Please enter a valid .csv file"
+      print_help
+      return
+    end
     begin
-      cohort = Cohort.new( File.basename(PATH, ".csv") )
-      CSV.foreach(PATH) do |row|
+      cohort = Cohort.new( File.basename(path, ".csv") )
+      prev_num_grades = false
+      CSV.foreach(path) do |row|
         student_name = row[0].split()
         student = Student.new(student_name[0], student_name[1])
         row.delete_at(0)
+        num_grades = 0
         row.each do |grade|
+          num_grades += 1
           student.accumulate_grade(grade)
         end
+        prev_num_grades ||= num_grades
+        if prev_num_grades != num_grades
+          puts "Error: entry #{student.full_name} has an unequal number of grades."
+          break
+        end
+        prev_num_grades = num_grades
         cohort.add_student(student)
       end
       cohort
-    rescue
+    rescue Exception => e
+      puts e.message
       puts "Error reading file"
+      return
     end
   end
 
 end
 
-c = Parser.parse_csv()
-c.to_txt_file ( {grades: true, averages: true, average_grades: :true, agg_average_score: :true, 
-                  agg_min_score: true, agg_max_score: true, agg_standard_deviation: true} )
+Interface.dispatch(ARGV)
